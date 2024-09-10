ad127cb61e101c5876070fb86781b53c1e36ddc3
Nina Lui <lui.n@northeastern.edu>
2024-09-10 15:34:25 -0700

    q4 - implemented when textinput blurs, input count text is replaced with input response message

--- components/Input.js
+++ components/Input.js
@@ -1,24 +1,33 @@
 import React, { useState } from 'react';
 import { TextInput, Text } from 'react-native';
 
-export default function Input( { focus }) {
+export default function Input({ focus }) {
     const [text, setText] = useState('');
     const [count, setCount] = useState(0);
+    const [isFocused, setIsFocused] = useState(focus);
     return (
         <>
-        <TextInput
-            autoCorrect={true}
-            keyboardType="default"
-            style={{ borderBottomColor: "purple", borderBottomWidth: 2 }}
-            placeholder="Enter text here"
-            value={text}
-            onChangeText={(changedText) => {
-                setText(changedText);
-                setCount(changedText.length);
-            }}
-            autoFocus={focus}
-        />
-        <Text>Character count: {count}</Text>
+            <TextInput
+                autoCorrect={true}
+                keyboardType="default"
+                style={{ borderBottomColor: "purple", borderBottomWidth: 2 }}
+                placeholder="Enter text here"
+                value={text}
+                onChangeText={(changedText) => {
+                    setText(changedText);
+                    setCount(changedText.length);
+                }}
+                autoFocus={focus}
+                onFocus={() => setIsFocused(true)}
+                onSubmitEditing={() => setIsFocused(false)}
+            />
+            {/* focus: show character count
+            blur: show message based on count < 3 */}
+            {isFocused ? (
+                count > 0 ? <Text>Character count: {count}</Text> : null
+            ) : (
+                count < 3 ? <Text>Please type more than 3 characters</Text> : <Text>Thank you</Text>
+            )}
         </>
     )
 }

6ac094e62432e8b26c5ef0b5758340dcece4b655
Nina Lui <lui.n@northeastern.edu>
2024-09-10 14:48:15 -0700

    added a header component and rendered in App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   const appName = "My app!";
   return (
     <View style={styles.container}>
-      <Text>Welcome to {appName}</Text>
+      {/* <Text>Welcome to {appName}</Text> */}
+      <Header name={appName} />
       <StatusBar style="auto" />
     </View>
   );

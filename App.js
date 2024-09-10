e1edaab40ff1a553470d2e1ad6be2e904b350975
Nina Lui <lui.n@northeastern.edu>
2024-09-10 14:54:50 -0700

    refactored TextInput out of App.js and into Input.js

--- App.js
+++ App.js
@@ -2,27 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Header from './components/Header';
+import Input from './components/Input';
 
 export default function App() {
   const appName = "My app!";
-  const [text, setText] = React.useState("");
 
   return (
     <View style={styles.container}>
       {/* <Text>Welcome to {appName}</Text> */}
       <Header name={appName} />
-
-      <TextInput
-        autoCorrect={true}
-        keyboardType="default"
-        style={{ borderBottomColor: "purple", borderBottomWidth: 2 }}
-        placeholder="Enter text here"
-        value={text}
-        onChangeText={(changedText) => {
-          setText(changedText);
-        }}
-      />
-
+      <Input />
       <StatusBar style="auto" />
     </View>
   );

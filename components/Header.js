5ee0aa0ef84efbaa7f1b61a379b41eac0f0c5531
Nina Lui <lui.n@northeastern.edu>
2024-09-17 14:59:47 -0700

    added styling to Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
 export default function Header( {name} ) {
     return (
         <View>
-            <Text>Welcome to {name}</Text>
+            <Text style={styles.text}>Welcome to {name}</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    text: {
+      color: 'purple',
+      fontSize: 20, 
+      borderWidth: 2,
+      borderRadius: 4, 
+      borderColor: 'purple',
+      padding: 5,
+      marginBottom: 5
+    }
+  });

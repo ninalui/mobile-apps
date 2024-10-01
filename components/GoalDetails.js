e71970569514a245788fbb2244c33e4e5a38194e
Nina Lui <lui.n@northeastern.edu>
2024-10-01 14:35:57 -0700

    added more details button to GoalDetails that pushes new screen, text conditionally rendered based on route.params

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,13 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
-export default function GoalDetails({ route }) {
-    const goal = route.params.goal;
+export default function GoalDetails({ navigation, route }) {
+
+    function moreDetailsHander() {
+        navigation.push('Details');
+    }
+
     return (
         <View>
-            <Text>Viewing goal details</Text>
-            <Text>Goal text: {goal.text}</Text>
-            <Text>Goal id: {goal.id}</Text>
+            {route.params ? (
+                <Text>Viewing goal details with text {route.params.goal.text} and id {route.params.goal.id}</Text>
+            ) : (
+                <Text>More details</Text>
+            )}
+
+            <Button
+                title="More Details"
+                onPress={moreDetailsHander}
+            />
         </View>
     )
 }

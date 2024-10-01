ca8008ace0af692a4a01173228a73311766f5d75
Nina Lui <lui.n@northeastern.edu>
2024-10-01 14:05:51 -0700

    added options prop to Home screen component to change title, background, and font color of header

--- App.js
+++ App.js
@@ -5,13 +5,21 @@ import Home from './components/Home'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import GoalDetails from './components/GoalDetails'
 
-const Stack = createNativeStackNavigator(); 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'My Goals',
+            headerStyle: { backgroundColor: 'purple' },
+            headerTintColor: 'white',
+          }}
+        />
         <Stack.Screen name="Details" component={GoalDetails} />
       </Stack.Navigator>
     </NavigationContainer>

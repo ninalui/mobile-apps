3e2c0748b789814659be319c300dfc99947adfd2
Nina Lui <lui.n@northeastern.edu>
2024-10-01 13:38:02 -0700

    created native stack navigator in App.js

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import Home from './components/Home'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator(); 
 
 export default function App() {
   return (
@@ -10,3 +13,4 @@ export default function App() {
     </NavigationContainer>
   );
 }
+

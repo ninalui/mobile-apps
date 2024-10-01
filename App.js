fd05cbc03a2028b3dc06a24a1934cbc26f9431b1
Nina Lui <lui.n@northeastern.edu>
2024-10-01 13:46:52 -0700

    added GoalDetails.js component and added as screen in App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import Home from './components/Home'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import GoalDetails from './components/GoalDetails'
 
 const Stack = createNativeStackNavigator(); 
 
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={GoalDetails} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ components/GoalDetails.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function GoalDetails({ navigation }) {
+  return (
+    <View>
+      <Text>GoalDetails</Text>
+    </View>
+  )
+}

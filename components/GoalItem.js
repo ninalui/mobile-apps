e3a8199261364448e4341d5e77fc173799e7eea5
Nina Lui <lui.n@northeastern.edu>
2024-10-08 14:57:16 -0700

    added long press on goal item to alert user if they want to delete goal and delete goal on confirm

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Alert } from "react-native";
 import PressableButton from "./PressableButton";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
@@ -13,10 +13,21 @@ export default function GoalItem({ goal, deleteHandler, navigation }) {
         navigation.navigate('Details', { goal: goal });
     };
 
+    function handleLongPress() {
+        Alert.alert('Delete', 'Are you sure you want to delete this goal?', [
+            { text: 'No', style: 'cancel', },
+            {
+                text: 'Yes',
+                onPress: () => { handleDelete() },
+            },
+        ]);
+    };
+
     return (
         <View style={styles.textContainer}>
             <Pressable
                 onPress={handlePress}
+                onLongPress={handleLongPress}
                 android_ripple={{ color: 'red', radius: 50 }}
                 style={({ pressed }) => [
                     styles.horizontalContainer,

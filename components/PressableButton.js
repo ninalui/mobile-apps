3ba06c525d869eb857d5fd8227f7520bd7d89f5a
Nina Lui <lui.n@northeastern.edu>
2024-10-08 14:20:20 -0700

    added default styling to PressableButton

--- components/PressableButton.js
+++ components/PressableButton.js
@@ -1,4 +1,4 @@
-import { Pressable, View } from 'react-native'
+import { Pressable, StyleSheet, View } from 'react-native'
 import React from 'react'
 
 export default function PressableButton({ children, componentStyle, pressHandler, pressedStyle }) {
@@ -6,7 +6,9 @@ export default function PressableButton({ children, componentStyle, pressHandler
         <Pressable
             onPress={pressHandler}
             style={({ pressed }) => [
+                styles.default,
                 componentStyle,
+                pressed && styles.defaultPressed,
                 pressed && pressedStyle,
             ]}
         >
@@ -16,3 +18,13 @@ export default function PressableButton({ children, componentStyle, pressHandler
         </Pressable>
     )
 }
+
+const styles = StyleSheet.create({
+    default: {
+        backgroundColor: 'blue', 
+    },
+    defaultPressed: {
+        opacity: 0.2,
+        backgroundColor: 'red'
+    }
+})

4abd2efa45137e142dc89c2e96eced20055c61af
Nina Lui <lui.n@northeastern.edu>
2024-10-29 14:11:51 -0700

    implemented creating new user in Signup

--- components/Signup.js
+++ components/Signup.js
@@ -1,5 +1,6 @@
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function Signup({ navigation }) {
     const [email, setEmail] = useState('');
@@ -8,6 +9,39 @@ export default function Signup({ navigation }) {
 
     const loginHandler = () => {
         navigation.replace('Login');
+    };
+
+    const signupHandler = async () => {
+        // validation: 
+        // 1. no fields are empty
+        if (!email || !password || !confirmPassword) {
+            alert('Please fill in all fields');
+            return;
+        }
+        // 2. password and confirm password match
+        if (password !== confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+        // 3. email is valid 
+        if (!email.includes('@') || !email.includes('.')) {
+            alert('Invalid email address');
+            return;
+        }
+        // 4. password is at least 6 characters long
+        if (password.length < 6) {
+            alert('Password must be at least 6 characters long');
+            return;
+        }
+        try {
+            const auth = getAuth();
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            console.log('User created');
+            navigation.replace('Home');
+        } catch (error) {
+            console.log('error creating user', error);
+        }
     };
 
     return (
@@ -39,7 +73,8 @@ export default function Signup({ navigation }) {
             />
 
             <Button
-                title="Login"
+                title="Signup"
+                onPress={signupHandler}
             />
 
             <Button

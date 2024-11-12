6e67fe9d2b280055daa08af8312cc7e54ca3973b
Nina Lui <lui.n@northeastern.edu>
2024-11-12 15:00:02 -0800

    showed marker when user taps on interactive map

--- components/Map.js
+++ components/Map.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { StyleSheet } from 'react-native';
 import MapView from "react-native-maps";
+import { Marker } from 'react-native-maps';
 
 export default function Map() {
+    const [selectedLocation, setSelectedLocation] = useState(null);
     return (
         <MapView
-            style={{ flex: 1 }}
+            style={styles.map}
             initialRegion={{
                 latitude: 37.78825,
                 longitude: -122.4324,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
             }}
-        />
+            onPress={event => {
+                setSelectedLocation({
+                    latitude: event.nativeEvent.coordinate.latitude,
+                    longitude: event.nativeEvent.coordinate.longitude
+                });
+            }}
+        >
+            <Marker coordinate={selectedLocation} />
+        </MapView>
     )
 }
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    }
+});
